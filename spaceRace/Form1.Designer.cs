b53ec745f0347bcca7934b374502d3a723defddd
Ivan Pokusaev <ivanpoku1762022@outlook.com>
2022-12-16 11:16:14 -0500

    Add project files.

---
+++ spaceRace/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace spaceRace
+{
+    partial class spaceRaceGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // spaceRaceGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "spaceRaceGame";
+            this.Text = "spaceRace";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ spaceRace/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spaceRace
+{
+    public partial class spaceRaceGame : Form
+    {
+        int playerSpeed = 5;
+        int playerSize = 48;
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        Rectangle player1 = new Rectangle(300, 400, 48, 48);
+        Rectangle player2 = new Rectangle(500, 400, 48, 48);
+        Image rocketImage = Properties.Resources.rocket_icon;
+
+        public spaceRaceGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(rocketImage, player1);
+            e.Graphics.DrawImage(rocketImage, player2);
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Player Move
+            if (wDown == true)
+            {
+                player1.Y -= playerSpeed;
+            }
+            if (sDown == true)
+            {
+                player1.Y += playerSpeed;
+            }
+            if (upArrowDown == true)
+            {
+                player2.Y -= playerSpeed;
+            }
+            if (downArrowDown == true)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            //Border Collisions
+            if (player1.Y <= 0)
+            {
+                player1.Y = 1;
+            }
+            if (player1.Y >= 465 - playerSize)
+            {
+                player1.Y = 465 - playerSize - 1;
+            }
+            if (player2.Y <= 0)
+            {
+                player2.Y = 1;
+            }
+            if (player2.Y >= 465 - playerSize)
+            {
+                player2.Y = 465 - playerSize - 1;
+            }
+            this.Refresh();
+        }
+    }
+}

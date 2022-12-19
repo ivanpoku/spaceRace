e8e66da740f168bae8d885b55cadfdefe8029669
Ivan Pokusaev <ivanpoku1762022@outlook.com>
2022-12-19 10:46:01 -0500

    sdfghjk

--- spaceRace/Form1.Designer.cs
+++ spaceRace/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // gameTimer
             // 
             this.gameTimer.Enabled = true;
-            this.gameTimer.Interval = 40;
+            this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
             // spaceRaceGame
--- spaceRace/Form1.cs
+++ spaceRace/Form1.cs
@@ -12,16 +12,21 @@ namespace spaceRace
 {
     public partial class spaceRaceGame : Form
     {
+        Random randGen = new Random();
         int playerSpeed = 5;
         int playerSize = 48;
+        int astroidSpeed = 10;
         bool wDown = false;
         bool sDown = false;
         bool upArrowDown = false;
         bool downArrowDown = false;
         Rectangle player1 = new Rectangle(300, 400, 48, 48);
         Rectangle player2 = new Rectangle(500, 400, 48, 48);
+        List<Rectangle> astroids = new List<Rectangle>();
         Image rocketImage = Properties.Resources.rocket_icon;
-
+        SolidBrush blackBrush = new SolidBrush(Color.White);
+        int generateRecValue;
+        int randValue;
         public spaceRaceGame()
         {
             InitializeComponent();
@@ -71,6 +76,10 @@ namespace spaceRace
         {
             e.Graphics.DrawImage(rocketImage, player1);
             e.Graphics.DrawImage(rocketImage, player2);
+            for (int i = 0; i < astroids.Count; i++)
+            {
+                e.Graphics.FillRectangle(blackBrush, astroids[i]);
+            }
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
@@ -109,6 +118,24 @@ namespace spaceRace
             if (player2.Y >= 465 - playerSize)
             {
                 player2.Y = 465 - playerSize - 1;
+            }
+
+            //Generate Astroids
+           
+           // randValue = randGen.Next(0, this.Height);
+
+            generateRecValue = randGen.Next(1, 101);
+            if (generateRecValue < 51)
+            {
+
+
+                astroids.Add(new Rectangle(0, randGen.Next(0, this.Height), 30, 10));
+                for (int i = 0; i < astroids.Count; i++)
+                {
+
+                    int x = astroids[i].X + astroidSpeed;
+                    astroids[i] = new Rectangle(x, astroids[i].Y, 30, 10);
+                }
             }
             this.Refresh();
         }
